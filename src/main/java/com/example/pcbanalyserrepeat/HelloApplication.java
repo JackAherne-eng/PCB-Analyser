a938991d047c8d029491f950e66fa5bdf498aa28
Jack Aherne <jack.aherne03@gmail.com>
2022-08-24 01:50:34 +0100

    Addition of image noise reduction

--- src/main/java/com/example/pcbanalyserrepeat/HelloApplication.java
+++ src/main/java/com/example/pcbanalyserrepeat/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,7 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PCB-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1518, 688);
+        stage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/pcbanalyserrepeat/PCBController.java
+++ src/main/java/com/example/pcbanalyserrepeat/PCBController.java
@@ -3,20 +3,35 @@ package com.example.pcbanalyserrepeat;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.*;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PCBController implements Initializable {
@@ -36,6 +51,11 @@ public class PCBController implements Initializable {
     public int[] imageSize;
     public Button process;
     public ImageView processedImage;
+    public AnchorPane rectangle;
+    public TextField rgbTolerance;
+    public TextArea disjointSetOuput;
+    public Label numofComp;
+    public Button rectangleBT;
 
     private PixelReader reader;
 
@@ -179,6 +199,87 @@ public class PCBController implements Initializable {
             }
     }
 
+    public void drawRectangle(MouseEvent mouseEvent) {
+
+        for (int i = 0; i < imageSize.length; i++) {
+            if (imageSize[i] != 0 && !roots.contains(Statics.disjointSet.find(imageSize, i))) {
+                roots.add(Statics.disjointSet.find(imageSize, i));
+            }
+        }
+
+        reduceNoise();
+
+        roots.sort((a, b) -> {
+            int acount = 0;
+            int bcount = 0;
+
+            for (int i = 0; i < imageSize.length; i++) {
+                if (Statics.disjointSet.find(imageSize, i) == a) {
+                    acount++;
+                }
+                if (Statics.disjointSet.find(imageSize, i) == b) {
+                    bcount++;
+                }
+            }
+            return acount - bcount;
+        });
+
+        deleteRect(processedImage);
+        int num = 0;
+        inputImage = imageDrop.getImage();
+
+        disjointSetOuput.clear();
+        for (int id : roots) {
+
+            num++;
+
+            double maxHeight = -1;
+            double minHeight = -1;
+            double toLeft = -1;
+            double toRight = -1;
+
+            for (int i = 0; i < imageSize.length; i++) {
+                int x = i % (int) inputImage.getWidth();
+                int y = i / (int) inputImage.getWidth();
+
+
+                if (imageSize[i] != 0 && Statics.disjointSet.find(imageSize, i) == id) {
+                    if (maxHeight == -1) {
+                        maxHeight = minHeight = y;
+                        toLeft = toRight = x;
+                    } else {
+                        if (x < toLeft) {
+                            toLeft = x;
+                        }
+                        if (x > toRight) {
+                            toRight = x;
+                        }
+                        if (y > minHeight) {
+                            minHeight = y;
+                        }
+                    }
+                }
+            }
+
+            disjointSetOuput.appendText("root ID -> " + id + ", Number of pixels -> " + countPixels(id, imageSize) + "\n");
+            Rectangle rect = new Rectangle(toLeft, maxHeight, toRight - toLeft, minHeight - maxHeight);
+            rect.setTranslateX(processedImage.getLayoutX());
+            rect.setTranslateY(processedImage.getLayoutY());
+            ((AnchorPane) processedImage.getParent()).getChildren().add(rect);
+            rect.setStroke(Color.YELLOWGREEN);
+            rect.setFill(Color.TRANSPARENT);
+            processedImage.setImage(inputImage);
+
+            Text text = new Text();
+            text.setFont(Font.font("Comic Sans", FontWeight.BOLD, FontPosture.REGULAR, 20));
+            text.setX(processedImage.getLayoutX() + toLeft);
+            text.setY(processedImage.getLayoutY() + maxHeight);
+            text.setText(String.valueOf(num));
+            ((AnchorPane) processedImage.getParent()).getChildren().add(text);
+        }
+        numofComp.setText(String.valueOf(num));
+    }
+
     /*
     Limitations weren't working properly with red values, so I had to use the following code to get it to work. This was achieved by getting
     help from Moses Ugwulo.
@@ -188,4 +289,44 @@ public class PCBController implements Initializable {
         return red > red1 - 0.15 && red < red1 + 0.15 && green > green1 - 0.15 && green < green1 + 0.15 && blue > blue1 - 0.15 && blue < blue1 + 0.15 && hue > hue1 - 2 && hue < hue1 + 2;
     }
 
+    /*
+    This method is used to reduce the amount of outstanding/ none usable pixels within the image. This was achieved by getting help from Moses Ugwulo.
+     */
+
+    public void reduceNoise(){
+
+        double noiseReduction = Double.parseDouble(rgbTolerance.getText());
+        double reduction = noiseReduction / 100;
+        roots.removeIf(integer -> (countPixels(integer, imageSize) / imageSize.length) * 100 < reduction);
+    }
+
+    private static double countPixels(int i, int[] a){
+        int pixels = 0;
+        for(int j = 0; j < a.length; j++){
+            if(i == Statics.disjointSet.find(a, j)){
+                pixels++;
+            }
+        }
+        return pixels;
+    }
+
+    public static void deleteRect(ImageView imageView){
+        List<Rectangle> list = new ArrayList<>();
+
+        for (Node node : ((AnchorPane) imageView.getParent()).getChildren()) {
+            if (node instanceof Rectangle) {
+                list.add((Rectangle) node);
+            }
+        }
+        ((AnchorPane) imageView.getParent()).getChildren().removeAll(list);
+
+        List<Text> textList = new ArrayList<>();
+
+        for (Node node : ((AnchorPane) imageView.getParent()).getChildren()) {
+            if (node instanceof Text) {
+                textList.add((Text) node);
+            }
+        }
+        ((AnchorPane) imageView.getParent()).getChildren().removeAll(textList);
+    }
 }
